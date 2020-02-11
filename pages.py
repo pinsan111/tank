2fc3b580c7650815f67e4348327e57eff7662265
geliang <63672310@qq.com>
2020-02-11 16:31:57 +0800

    抽象类实现

--- pages.py
+++ pages.py
@@ -1,5 +1,7 @@
 from pygame.locals import *
 from abc import *
+import pygame
+
 '''
 当前显示页面
 '''
@@ -26,38 +28,54 @@ class Page(metaclass=ABCMeta):
     需要吧page做成抽象类 abstract class
     起规范作用，自身不可实例化
     '''
-    pass
-
-
-class FirstPage:
-
     def __init__(self,surface):
-        self.surface=surface
+        if isinstance(surface,pygame.Surface):
+            self.surface=surface
+
+    @abstractmethod
     def render(self):
-        self.surface.fill((0xff,0,0))
-    def key_down(self,key):
         '''
-        key down事件
-        :param key:用户按下的键
+        渲染页面，所有子类必须实现
         :return:
         '''
+    def key_down(self,key):
+        '''
+        键盘按下事件，非必需，不用定义成抽象方法，子类可选择复写函数
+        :param key:按下的键
+        :return:
+        '''
+
+
+
+class FirstPage(Page):
+
+    def __init__(self, surface):
+        super().__init__(surface)
+
+    def render(self):
+        self.surface.fill((0xff,0,0))
+
+    def key_down(self,key):
         if key==K_RETURN:
             go(SecondPage(self.surface))
 
-        pass
 
 
-class SecondPage:
+class SecondPage(Page):
 
-    def __init__(self,surface):
-        self.surface=surface
+    def __init__(self, surface):
+        super().__init__(surface)
+
     def render(self):
         self.surface.fill((0,0xff,0))
+
+    def key_down(self, key):
         '''
         key down事件
         :param key:用户按下的键
         :return:
         '''
+        print('2')
 
 
         pass
