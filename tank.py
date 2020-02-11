23a15feb45e51885f7330beb7520a8e9c0bd8aba
geliang <63672310@qq.com>
2020-02-11 14:18:34 +0800

    FPS计算

--- tank.py
+++ tank.py
@@ -1,19 +1,36 @@
 import pygame
 from pygame.locals import *
 import sys
+import time
 
+from locals import *
 
 if __name__ == '__main__':
     #游戏初始化
     pygame.init()
     #设置窗体
     pygame.display.set_mode((600,600))
+    #FPS值
+    fps=0
     while True:
+        #获取开始时间戳
+        start=time.time()
+        #事件
         events=pygame.event.get()
         for event in events:
             #窗体关闭
             if event.type == QUIT:
                 pygame.quit()
                 sys.exit(0)
+        end=time.time()
+        #获取耗时
+        cost=end-start
+        if cost<DEFAULT_DELAY:
+            sleep=DEFAULT_DELAY-cost
+        else:
+            sleep=0
+        time.sleep(sleep)
+        fps=1.0/(end-start)
+
 
 
